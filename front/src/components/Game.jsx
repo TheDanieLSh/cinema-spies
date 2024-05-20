668db269e55b62decebb0dd3f433b06128ef7128
Danila Shuvaev <dshuvaev@art3d.ru>
2024-05-20 18:09:32 +0300

    f5

--- front/src/components/Game.jsx
+++ front/src/components/Game.jsx
@@ -2,7 +2,10 @@ export default function Game() {
 
     return (
         <>
-
+            <form name="players">
+                <label>Введите количество игроков</label>
+                <input type="number" autocomplete="off"></input>
+            </form>
         </>
     )
 }
